f749be9aaa206c041005d2eaeaf29752033965d3
ZaxarShashkov <zekharjah@gmail.com>
2023-01-29 21:46:48 +0300

    up

---
+++ HomeWork/tennis.js
@@ -0,0 +1,25 @@
+const app = document.querySelector('#app');
+
+
+const table = document.createElement('div');
+table.className = 'table';
+table.style.width = 400 + 'px';
+table.style.height = 200 + 'px';
+app.appendChild(table);
+
+const racquet = document.createElement('div');
+racquet.className = 'racquet';
+table.appendChild(racquet);
+
+const racquet2 = document.createElement('div');
+racquet2.className = 'racquet2';
+table.appendChild(racquet2);
+
+const ball = document.createElement('div');
+ball.className = 'ball';
+table.appendChild(ball);
+
+const moveDown = () => {
+    
+}
+

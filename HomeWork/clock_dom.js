d8acd3e3842db68cfaf90dcb6511fec69198e80c
ZaxarShashkov <zekharjah@gmail.com>
2023-01-29 21:26:56 +0300

    up

--- HomeWork/clock_dom.js
+++ HomeWork/clock_dom.js
@@ -2,4 +2,25 @@ const app = document.querySelector('#app');
 
 const createClock = () => {
 
+    // Создаем циферблат 
+    const clock = document.createElement('div');
+    clock.className = 'clock';
+    app.appendChild(clock);
+
+    const height = clock.offsetHeight;
+    console.log(height)
+
+    // Создаем циферблат 
+    for (let i = 1; i <= 12; i++) {
+        const blockNumbers = document.createElement('div');
+        blockNumbers.className = 'block_numbers';
+        clock.appendChild(blockNumbers);
+        blockNumbers.style.left = height/2 - 25 + 'px';
+    }
+
+
+    
+
 }
+
+createClock();

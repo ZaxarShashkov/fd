039afc2fad672558acd257e2f7c0f87e9e4eb221
ZaxarShashkov <zekharjah@gmail.com>
2023-01-31 09:28:50 +0300

    up

--- HomeWork/clock_dom.js
+++ HomeWork/clock_dom.js
@@ -8,18 +8,26 @@ const createClock = () => {
     app.appendChild(clock);
 
     const height = clock.offsetHeight;
+    const width = clock.offsetWidth;
+
+    console.log(width)
     console.log(height)
+    let deg = 30;
 
     // Создаем циферблат 
     for (let i = 1; i <= 12; i++) {
         const blockNumbers = document.createElement('div');
         blockNumbers.className = 'block_numbers';
         clock.appendChild(blockNumbers);
-        blockNumbers.style.left = height/2 - 25 + 'px';
+        blockNumbers.style.cssText = `transform: rotate(${deg}deg)`;
+        blockNumbers.style.left = width - 215 + 'px';
+        blockNumbers.style.top = height - 390+ 'px'
+        blockNumbers.style.WebkitTransformOrigin = (width / 20 - 3) + 'px' + " " + (width / 2 - 10) + 'px';
+        deg += 30;
     }
 
 
-    
+
 
 }
 
--- HomeWork/tennis.js
+++ HomeWork/tennis.js
@@ -1,9 +1,8 @@
 const app = document.querySelector('#app');
 
-let ballY = 90;
-let ballX = 190;
-
 let isGameOver = true;
+let ballX = 190;
+let ballY = 95;
 
 const table = document.createElement('div');
 table.className = 'table';
@@ -15,67 +14,111 @@ let y = 0;
 
 const racquet = document.createElement('div');
 racquet.className = 'racquet';
+racquet.style.top = 10 + 'px';
 table.appendChild(racquet);
 
 const racquet2 = document.createElement('div');
 racquet2.className = 'racquet2';
+racquet2.style.top = 10 + 'px';
 table.appendChild(racquet2);
 
 const ball = document.createElement('div');
 ball.className = 'ball';
 table.appendChild(ball);
-ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+ball.style.left = ballX + 'px';
+ball.style.top = ballY + 'px';
+
 
 
 window.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
         case 16:
-            racquet.style.top = (y -= 5) + 'px';
+            racquet.style.top = y + 'px';
+            y -= 5;
+            if (racquet.offsetTop <= 5) {
+                racquet.style.top = y + 'px';
+                y += 5;
+            }
             break;
         case 17:
-            racquet.style.top = (y += 5) + 'px';
+            racquet.style.top = y + 'px';
+            y += 5;
+            if (racquet.offsetTop >= 130) {
+                racquet.style.top = y + 'px';
+                y -= 5;
+            }
             break;
+
         case 38:
-            racquet2.style.top = (y -= 5) + 'px';
+            racquet2.style.top = y + 'px';
+            y -= 5;
+            if (racquet2.offsetTop <= 5) {
+                racquet2.style.top = y + 'px';
+                y += 5;
+            }
             break;
         case 40:
-            racquet2.style.top = (y += 5) + 'px';
+            racquet2.style.top = y + 'px';
+            y += 5;
+            if (racquet2.offsetTop >= 130) {
+                racquet2.style.top = y + 'px';
+                y -= 5;
+            }
             break;
     }
 });
 
-let isMovingLeft;
-let isMovingRight;
-
 
 
 const moveLeft = () => {
-    ballY += 0;
-    ballX -= 1;
-    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
-    if (ballX <= 0) {
-        window.cancelAnimationFrame(isMovingLeft);
-        isMovingRight = window.requestAnimationFrame(moveRight);
+    clearInterval(moveRight)
+    const ball = document.querySelector('.ball');
+    ball.style.left = ballX + 'px';
+    ballX -= 2;
+    if (ballX <= 5) {
+        setInterval(moveRight,30);
     }
-    isMovingLeft = window.requestAnimationFrame(moveLeft);
 }
-
 
 const moveRight = () => {
-    ballY += 0;
-    ballX += 1;
-    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
-    if (ballX >= 380) {
-        window.cancelAnimationFrame(isMovingRight);
-        isMovingLeft = window.requestAnimationFrame(moveLeft);
+    clearInterval(moveLeft)
+    const ball = document.querySelector('.ball');
+    ball.style.left = ballX + 'px';
+    ballX += 2;
+    if(ballX >= 380) {
+        setInterval(moveLeft,30)
     }
-    isMovingRight = window.requestAnimationFrame(moveRight);
 }
-
 
 
 const start = () => {
     isGameOver = false;
-    isMovingRight = window.requestAnimationFrame(moveRight);
+    setInterval(moveLeft, 30);
 }
 start()
+
+
+
+
+// const moveLeft = () => {
+//         ballY += 0;
+//         ballX -= 1;
+//         ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+//         if (ballX <= 10) {
+//             window.cancelAnimationFrame(isMovingLeft);
+//             isMovingRight = window.requestAnimationFrame(moveRight);
+//         }
+//         isMovingLeft = window.requestAnimationFrame(moveLeft);
+//     }
+
+
+//     const moveRight = () => {
+//         ballY += 0;
+//         ballX += 1;
+//         ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+//         if (ballX >= 390) {
+//             window.cancelAnimationFrame(isMovingRight);
+//             isMovingLeft = window.requestAnimationFrame(moveLeft);
+//         }
+//         isMovingRight = window.requestAnimationFrame(moveRight);
+//     }
